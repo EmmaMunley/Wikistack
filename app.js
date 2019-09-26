1712f45f303821927b86527d64d248f793bdb122
Emma Munley <emmamunley@gmail.com>
2019-09-26 14:49:26 -0400

    added models and synced them with db

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
-// requires node modules express and morgan 
-const express = require('express')
-const morgan = require("morgan");
-// creating variable 'app' that renders our page using express 
-const app = express()
+// requires node modules express and morgan
+const express = require('express');
+const morgan = require('morgan');
+
+// creating variable 'app' that renders our page using express
+const app = express();
+
+//connect db from index.js using sequelize
+const models = require('./models');
+
+//verify that connection to db works
+// db.authenticate().then(() => {
+//   console.log('connected to the database');
+// });
+
 // parses url-encoded bodies
 app.use(express.urlencoded({ extended: false }));
 // parses json bodies
 app.use(express.json());
 // adds the console.logs for each request
-app.use(morgan("dev"));
-// serves up static files from public folder 
-app.use(express.static(__dirname + "/public"));
+app.use(morgan('dev'));
+// serves up static files from public folder
+app.use(express.static(__dirname + '/public'));
 
 const html = `
 <!DOCTYPE html>
 <html>
 <head>
   <title>Wikistack</title>
-  <link rel="stylesheet" href="/stylesheets/style.css" /> 
+  <link rel="stylesheet" href="/stylesheets/style.css" />
 </head>
 <body>
   <h1>Hello World!</h1>
 </body>
 </html>
-`
+`;
 // root directory will render the following
-app.get("/", (req, res, next) => {
-    res.send(html)
-  })
+app.get('/', (req, res, next) => {
+  res.send(html);
+});
 
-  const PORT = 3000
+// async .sync mothod
+const sync = async () => {
+  await models.db.sync();
+};
 
-  app.listen(PORT, () => {
-    console.log(`App listening in port ${PORT}`);
-  });
+sync({ force: true });
+
+//port
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
---
+++ models/index.js
@@ -0,0 +1,43 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+//create the model/table for page
+const Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    isUrl: true,
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+  },
+});
+
+//create the model/table for users
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
+
+module.exports = {
+  db,
+  Page,
+  User,
+};
